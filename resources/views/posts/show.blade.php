8f7937bb3a9931353e5f398aa164798101a60ef6
Popinho96 <antonio.popic@gmail.com>
2019-03-04 19:02:56 +0100

    second commit

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+            <li>{{ $post->title }}</li>
+            <br>
+            <section>{{ $post->body }}</section>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Post;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/* Route::get('/about', function () {
+    $user = array(
+        'name'      =>  'Toni',
+        'age'       =>   23,
+        'job'       =>  'unemployed',
+        'status'    => 'online'
+    );
+                        //['user' => $user]
+    //return view('about', compact('user'))/*->with(['user' => $user]);
+}); */
+Route::get('/posts', 'PostsController@index');
+
+Route::get('/posts/{id}', 'PostsController@show');
+
+
+/* 
+Route::get('/users', 'UsersController@index');
+
+Route::get('/users/create', 'UsersController@create')->name('users.create');
+
+Route::post('/users', 'UsersController@store');
+
+Route::get('/users/{user}', 'UsersController@show');
+
+Route::get('/users/{user}/edit', 'UsersController@edit');
+
+Route::put('/users/{user}', 'UsersController@update');
+
+Route::delete('/users/{user}', 'UsersController@destroy'); */
+
+Route::resource('users', 'UsersController');
